c39a11874813051861e8a60d1fd51840b1518feb
acatzk <joshuaimalay@gmail.com>
2022-05-03 13:28:41 +0800

    fix(style): add mobile responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import React from 'react'
 import Header from 'components/Header'
+import Footer from 'components/Footer'
 
 const Index: NextPage = () => {
   return (
@@ -10,6 +11,8 @@ const Index: NextPage = () => {
         <title>Welcome to Davao Metro Shuttle</title>
       </Head>
       <Header />
+      <main className="min-h-[91vh]"></main>
+      <Footer />
     </React.Fragment>
   )
 }
