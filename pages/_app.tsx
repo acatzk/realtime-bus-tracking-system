4247d2532c22f87ec4331871a15e7a9d8bd04c98
acatzk <joshuaimalay@gmail.com>
2022-05-03 11:54:46 +0800

    chore: clean up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
 
