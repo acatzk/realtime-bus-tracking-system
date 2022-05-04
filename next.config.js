385e1ee6c0107747869e476667c5f6a5819d55bb
acatzk <joshuaimalay@gmail.com>
2022-05-04 17:37:07 +0800

    chore: remove unused env.

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    TOM_TOM_API_KEY: process.env.TOM_TOM_API_KEY,
-    NEXT_PUBLIC_NHOST_BACKEND: process.env.NEXT_PUBLIC_NHOST_BACKEND,
-    NEXT_GOOGLE_MAP_API_KEY: process.env.NEXT_GOOGLE_MAP_API_KEY
+    NEXT_PUBLIC_NHOST_BACKEND: process.env.NEXT_PUBLIC_NHOST_BACKEND
   },
   images: {
     domains: [
