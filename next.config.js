9f0ab690ef76bc999b7503789a326409ae768a3e
acatzk <joshuaimalay@gmail.com>
2022-05-04 08:30:50 +0800

    feat: add bus schedule table (#3)

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     TOM_TOM_API_KEY: process.env.TOM_TOM_API_KEY
-  }
+  },
+  images: {
+    domains: [
+      "th.bing.com",
+    ],
+  },
 }
 
 module.exports = nextConfig
