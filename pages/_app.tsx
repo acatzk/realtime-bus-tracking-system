58afff98981ce4cbc359cc7e391cb460d57bd784
acatzk <joshuaimalay@gmail.com>
2022-05-27 15:13:54 +0800

    feat: add progress indicator per click

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,13 @@ import { nhost } from 'lib/nhost-client'
 import { NhostNextProvider } from '@nhost/nextjs'
 import { ToastContainer } from 'react-toastify'
 import { NhostApolloProvider } from '@nhost/react-apollo'
+import NextProgress from 'utils/next-progress'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <NhostNextProvider nhost={nhost} initial={pageProps.nhostSession}>
       <NhostApolloProvider nhost={nhost}>
+        <NextProgress />
         <ToastContainer />
         <Component {...pageProps} />
       </NhostApolloProvider>
