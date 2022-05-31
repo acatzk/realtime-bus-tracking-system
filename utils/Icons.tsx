145b324fa0216ee6dc246fa25ed0d3112826ecd5
acatzk <joshuaimalay@gmail.com>
2022-05-31 21:16:58 +0800

    chore: change svg loading spinner

--- utils/Icons.tsx
+++ utils/Icons.tsx
@@ -6,20 +6,20 @@ export const Spinner: React.FC<IconName> = ({ className }) => {
   return (
     <svg
       className={className}
-      viewBox="0 0 24 24"
+      viewBox="0 0 42 42"
       xmlns="http://www.w3.org/2000/svg"
-      fill="currentColor">
-      <g>
-        <path d="M10.998 22a.846.846 0 010-1.692 9.308 9.308 0 000-18.616 9.286 9.286 0 00-7.205 3.416.846.846 0 11-1.31-1.072A10.978 10.978 0 0110.998 0c6.075 0 11 4.925 11 11s-4.925 11-11 11z"></path>
-        <animateTransform
-          attributeName="transform"
-          attributeType="XML"
-          type="rotate"
-          from="0 11 11"
-          to="360 11 11"
-          dur=".6s"
-          calcMode="linear"
-          repeatCount="indefinite"></animateTransform>
+      stroke="currentColor">
+      <g transform="translate(3 3)" strokeWidth="4" fill="none" fillRule="evenodd">
+        <circle strokeOpacity=".5" cx="18" cy="18" r="18"></circle>
+        <path d="M36 18c0-9.94-8.06-18-18-18">
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            from="0 18 18"
+            to="360 18 18"
+            dur="0.7s"
+            repeatCount="indefinite"></animateTransform>
+        </path>
       </g>
     </svg>
   )
