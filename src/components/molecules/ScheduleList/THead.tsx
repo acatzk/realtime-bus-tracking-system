bee793e03154d525759e5b9da7c2b78d18909de8
Joshua Galit <108642414+jsvelte@users.noreply.github.com>
2023-05-05 08:10:04 +0800

    [RTBTS-ffe]-[FE] Switch Destination and Drivers Column (#49)
    Co-authored-by: acatzk <joshuaimalay@gmail.com>

--- src/components/molecules/ScheduleList/THead.tsx
+++ src/components/molecules/ScheduleList/THead.tsx
@@ -4,80 +4,80 @@ export function THead() {
       <tr className="bg-secondary text-center">
         <th
           className="
-      w-1/3
-      min-w-[160px]
-      border-l
-      border-transparent
-      py-4
-      px-3
-      text-base
-      font-semibold
-      text-white
-      md:text-lg
-      lg:py-7 lg:px-4
-    ">
-          Driver
-        </th>
-        <th
-          className="
-      w-1/3
-      min-w-[160px]
-      border-l
-      border-transparent
-      py-4
-      px-3
-      text-base
-      font-semibold
-      text-white
-      md:text-lg
-      lg:py-7 lg:px-4
-    ">
+            w-1/3
+            min-w-[160px]
+            border-l
+            border-transparent
+            py-4
+            px-3
+            text-base
+            font-semibold
+            text-white
+            md:text-lg
+            lg:py-7 lg:px-4
+          ">
           Destination
         </th>
         <th
           className="
-      w-1/3
-      min-w-[160px]
-      border-l
-      border-transparent
-      py-4
-      px-3
-      text-base
-      font-semibold
-      text-white
-      md:text-lg
-      lg:py-7 lg:px-4
-    ">
+            w-1/3
+            min-w-[160px]
+            border-l
+            border-transparent
+            py-4
+            px-3
+            text-base
+            font-semibold
+            text-white
+            md:text-lg
+            lg:py-7 lg:px-4
+          ">
+          Driver
+        </th>
+        <th
+          className="
+            w-1/3
+            min-w-[160px]
+            border-l
+            border-transparent
+            py-4
+            px-3
+            text-base
+            font-semibold
+            text-white
+            md:text-lg
+            lg:py-7 lg:px-4
+          ">
           Departure Time
         </th>
         <th
           className="
-      w-1/3
-      min-w-[160px]
-      py-4
-      px-3
-      text-base
-      font-semibold
-      text-white
-      md:text-lg
-      lg:py-7
-      lg:px-4
-    ">
+            w-1/3
+            min-w-[160px]
+            py-4
+            px-3
+            text-base
+            font-semibold
+            text-white
+            md:text-lg
+            lg:py-7
+            lg:px-4
+          ">
           Plate Number
         </th>
         <th
           className="
-      w-1/6
-      min-w-[160px]
-      py-4
-      px-3
-      text-base
-      font-semibold
-      text-white
-      md:text-lg
-      lg:py-7
-      lg:px-4
-    ">
+            w-1/6
+            min-w-[160px]
+            py-4
+            px-3
+            text-base
+            font-semibold
+            text-white
+            md:text-lg
+            lg:py-7
+            lg:px-4
+          ">
           Track Location
         </th>
       </tr>
